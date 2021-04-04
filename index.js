67cf87840583c1c13e892b7223863568067a828a
Shikhar Agrawal <shikhar2307@yahoo.in>
2021-04-04 22:57:25 +0530

    hamburger added for mobile view

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ for(let i = 0; i<document.querySelectorAll(".measure-bar").length; i++){
 // });
 }
 function fillUp(e){
-e.style = "background-color:green"
+e.style = "background-color:green";
 }
 
+document.querySelector(".hamburger").addEventListener("click", function(){
+    var menu = document.querySelector(".menu");
+    var burger = document.querySelector(".hamburger");
+    if(menu.style.display == "none"){
+        menu.style.display = "flex";
+        burger.style.background = "orange";
+    }
+    else{
+         menu.style.display = "none";
+         burger.style.background = "#171c5d";
+    }
+    
+});
